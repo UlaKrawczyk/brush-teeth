f5f03325f6a9d4cdfa2dbd8e49314701bf455451
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-10-06 00:52:37 +0200

    feat: js functionality without localStorage for images, just number of brushes

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -15,8 +15,34 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
+const checkButton = document.querySelector('.play__button--js');
+const checkNumber = document.querySelector('.play__number--js');
+const images = document.querySelectorAll('.rewards__wrapper img');
+let number = 0;
 
+let randomNumbers = [];
+for (let i = 0; i < images.length; i++) {
+  randomNumbers.push(i);
+}
 
-console.log(`Hello world!`);
+if (!localStorage.getItem(checkNumber)) {
+  localStorage.setItem(checkNumber, number);
+  checkNumber.innerHTML = number;
 
+} else {
+  checkNumber.innerHTML = localStorage.getItem(checkNumber);
+  number = localStorage.getItem(checkNumber);
+}
 
+checkButton.addEventListener('click', () => {
+  number++;
+  checkNumber.innerHTML = number;
+  localStorage.setItem(checkNumber, number);
+
+  if (number == 1 || number == 3 || number == 5 || number == 10 || number == 12 || number == 15 || number == 17 || number == 20 || number == 22 || number == 25 || number == 27 || number == 30) {
+    let randomNum = Math.floor(Math.random() * randomNumbers.length);
+    let imgNum = randomNumbers[randomNum];
+    images[imgNum].classList.add('visible');
+    randomNumbers.splice(randomNum, 1);
+  }
+});
