058a8e904d36f2afd19dbe76c106c6a3dc596fc8
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-10-09 15:26:12 +0200

    feat: images shows one after another

--- src/js/test.js
+++ src/js/test.js
@@ -17,9 +17,11 @@ const checkButton = document.querySelector('.play__button--js');
 const brushNumber = document.querySelector('.play__number--js');
 const images = document.querySelectorAll('.rewards__wrapper img');
 let number = 0;
-let imageNumber = 0;
 let imagesNumbers = [];
-
+let randomNumbers = [];
+for (let i = 0; i < images.length; i++) {
+  randomNumbers.push(i);
+}
 
 if (!localStorage.getItem('brushNumber')) {
   localStorage.setItem('brushNumber', number);
@@ -32,7 +34,10 @@ if (!localStorage.getItem('brushNumber')) {
   for (let num of imagesNumbers) {
     images[num].classList.add('visible');
   }
-  imageNumber = parseInt(localStorage.getItem('imageNumber'));
+  for (let num of imagesNumbers) {
+    randomNumbers.splice(num, 1);
+  }
+  console.log(randomNumbers);
 }
 
 checkButton.addEventListener('click', () => {
@@ -41,11 +46,15 @@ checkButton.addEventListener('click', () => {
   localStorage.setItem('brushNumber', number);
 
   if (number == 1 || number == 3 || number == 5 || number == 10 || number == 12 || number == 15 || number == 17 || number == 20 || number == 22 || number == 25 || number == 27 || number == 30) {
+    let randomNum = Math.floor(Math.random() * randomNumbers.length);
+    console.log(randomNum);
+    let imgNum = randomNumbers[randomNum];
+    console.log(imgNum);
 
-    images[imageNumber].classList.add('visible');
-    imagesNumbers.push(imageNumber);
+    images[imgNum].classList.add('visible');
+    randomNumbers.splice(randomNum, 1);
+
+    imagesNumbers.push(imgNum);
     localStorage.setItem('imagesNumbers', JSON.stringify(imagesNumbers));
-    imageNumber++;
-    localStorage.setItem('imageNumber', imageNumber);
   }
 });
